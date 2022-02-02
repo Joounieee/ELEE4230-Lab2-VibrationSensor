e111b45d4f9301037b04be9cf25bb57dcc841543
Juan Gabriel Quesada Rivas <jqr46095@csci-odin.cs.uga.edu>
2022-02-02 14:23:26 -0500

    added y-axis sensing and output

--- vibration_sensor.ino
+++ vibration_sensor.ino
@@ -5,64 +5,110 @@
 int analog_Z = A0;
 int analog_Y = A1;
 
-int analogVibArr[VIB_ARR_SIZE];
+int analogVibArrZ[VIB_ARR_SIZE];
+int analogVibArrY[VIB_ARR_SIZE];
 
 int z_max;
 int z_min;
 int y_max;
 int y_min;
 
-int vibAmpl; // used for intemediate calculation of vib
+int vibAmplZ;
+int vibAmplY;
 
 int threshold = 70;
 
 unsigned long last_us = 0L;
 
+int counter = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(LED,OUTPUT);
+  delay(2000);    // waits 2 secs before calibration
+  /*
+  int tf_calib_Z[10];
+  int tf_calib_Y[10];
+  for (int i = 0; i < 10; i++) {
+    tf_calib_Z[i] = analogRead(analog_Z);
+    tf_calib_Y[i] = analogRead(analog_Y);
+  } // for
 
+  int sum_Z = 0;
+  int sum_Y = 0;
+  for (int i = 0; i < 10; i++) {
+    sum_Z += tf_calib_Z[i];
+    sum_Y += tf_calib_Y[i]; 
+  } // for
+
+  int avg_Z = sum_Z / 10;
+  int avg_Y = sum_Y / 10;
+
+  */
+  
+  
 } // setup
 
 void loop() {
-
-  if (micros() - last_us > PERIOD) {
+  counter = 0;
+  while (counter < VIB_ARR_SIZE) {
+    if (micros() - last_us > PERIOD) {
        last_us += PERIOD;
        sample();
-  } // if
-
-  z_max = analogVibArr[0];
-  z_min = analogVibArr[0];
+    } // if
+    
+    // output on serial monitor
+    Serial.print("Analog Signal Z-axis (in mV): ");
+    Serial.println(analogVibArrZ[counter]);
+    Serial.print("Analog Signal Y-axis (in mV): ");
+    Serial.println(analogVibArrY[counter]);
+    counter++;
+  } // while
+  
+  z_max = analogVibArrZ[0];
+  z_min = analogVibArrZ[0];
+  y_max = analogVibArrY[0];
+  y_min = analogVibArrY[0];
 
   // finding max
   for (int i = 1; i < VIB_ARR_SIZE; i++) {
-    z_max = max(analogVibArr[i],z_max);
+    z_max = max(analogVibArrZ[i],z_max);
+    y_max = max(analogVibArrY[i],y_max);
   } // for
 
   // finding min
   for (int i = 1; i < VIB_ARR_SIZE; i++) {
-    z_min = min(analogVibArr[i],z_min);
+    z_min = min(analogVibArrZ[i],z_min);
+    y_min = min(analogVibArrY[i],y_min);
   } // for
 
   // find abs of diff
-  vibAmpl = abs(z_max - z_min);
+  vibAmplZ = abs(z_max - z_min);
+  vibAmplY = abs(y_max - y_min);
+
+  // outputting vibAmpl
+  for (int i = 0; i < 20; i++) {
+    Serial.print("vibAmplZ: ");
+    Serial.println(vibAmplZ);
+    Serial.print("vibAmplY: ");
+    Serial.println(vibAmplY);    
+  } // for
 
   // checking if vib is above threshold
-  if (vibAmpl > threshold) {
-    Serial.print("Vibration Amplitude: ");
-    Serial.println(vibAmpl);
+  if ((vibAmplZ || vibAmplY) > threshold) {
+    Serial.print("Vibration Amplitude (Z): ");
+    Serial.println(vibAmplZ);
+    Serial.print("Vibration Amplitude (Y): ");
+    Serial.println(vibAmplY);
     digitalWrite(LED,HIGH);
     delay(1000);      
     digitalWrite(LED,LOW);
 
-  }
+  } // if
 
 } // loop
 
 void sample() {
-  for (int i = 0; i < VIB_ARR_SIZE; i++) {
-    analogVibArr[i] = analogRead(analog_Z);
-    Serial.print("Analog Signal (in mV): ");
-    Serial.println(analogVibArr[i]);
-  } // for
+  analogVibArrZ[counter] = analogRead(analog_Z);
+  analogVibArrY[counter] = analogRead(analog_Y);
 } // sample
